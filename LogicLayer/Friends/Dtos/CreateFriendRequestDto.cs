447208bbfda95455d7c27b535393e4791aad4c28
V0ldek <registermen@gmail.com>
2019-04-02 18:41:30 +0200

    Added a request link generator delegate to CreateFriendRequestDto.

--- LogicLayer/Friends/Dtos/CreateFriendRequestDto.cs
+++ LogicLayer/Friends/Dtos/CreateFriendRequestDto.cs
@@ -5,15 +5,27 @@ namespace GameBoard.LogicLayer.Friends.Dtos
     public sealed class CreateFriendRequestDto
     {
         [NotNull]
+        public delegate string RequestLinkGenerator([NotNull] string id);
+
+        private readonly RequestLinkGenerator _requestLinkGenerator;
+
+        [NotNull]
         public string UserNameFrom { get; }
 
         [NotNull]
         public string UserNameTo { get; }
 
-        public CreateFriendRequestDto([NotNull] string userNameFrom, [NotNull] string userNameTo)
+        public CreateFriendRequestDto(
+            [NotNull] string userNameFrom,
+            [NotNull] string userNameTo,
+            [NotNull] RequestLinkGenerator requestLinkGenerator)
         {
             UserNameFrom = userNameFrom;
             UserNameTo = userNameTo;
+            _requestLinkGenerator = requestLinkGenerator;
         }
+
+        [NotNull]
+        public string GenerateRequestLink([NotNull] string id) => _requestLinkGenerator(id);
     }
 }
