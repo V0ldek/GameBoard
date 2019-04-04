3505fceaf7d98478b46bbbc609fe08d11c7d9342
V0ldek <registermen@gmail.com>
2019-04-04 23:56:39 +0200

    Added bundling and minification.

--- GameBoard/gulpfile.js
+++ GameBoard/gulpfile.js
@@ -4,10 +4,16 @@ This file is the main entry point for defining Gulp tasks and using Gulp plugins
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 */
 
-var gulp = require("gulp");
-var del = require("del");
+const gulp = require("gulp");
+const del = require("del");
+const concat = require("gulp-concat");
+const cleanCss = require("gulp-clean-css");
+const terser = require("gulp-terser");
+const merge = require("merge-stream");
+const bundleconfig = require("./bundleconfig.json");
+const gutil = require("gulp-util");
 
-var paths = {
+const paths = {
     scripts: ["Scripts/**/*.js", "Scripts/**/*.ts", "Scripts/**/*.map"],
     libs: [
         "node_modules/bootstrap/**/*",
@@ -21,17 +27,51 @@ var paths = {
     ]
 };
 
-gulp.task("lib", function() {
-    gulp.src(paths.libs, { base: "node_modules" }).pipe(gulp.dest("wwwroot/lib/"));
+gulp.task("lib:all",
+    () => {
+        return gulp.src(paths.libs, { base: "node_modules" }).pipe(gulp.dest("wwwroot/lib/"));
+    });
 
-    // Fix for Boostrap being dumb and not seeing the type definition in popper.js.
-    gulp.src("node_modules/popper.js/index.d.ts", { base: "node_modules" }).pipe(gulp.dest("node_modules/@types/"));
-});
+gulp.task("lib:popperFix",
+    () => {
+        // Fix for Boostrap being dumb and not seeing the type definition in popper.js.
+        return gulp.src("node_modules/popper.js/index.d.ts", { base: "node_modules" })
+            .pipe(gulp.dest("node_modules/@types/"));
+    });
 
-gulp.task("clean", function () {
-    return del(["wwwroot/scripts/**/*"]);
-});
+gulp.task("lib", gulp.series(["lib:all", "lib:popperFix"]));
 
-gulp.task("default", function () {
-    gulp.src(paths.scripts).pipe(gulp.dest("wwwroot/scripts/"));
-});
+gulp.task("clean",
+    () => {
+        return del(["wwwroot/scripts/**/*"]);
+    });
+
+gulp.task("min:js",
+    () => {
+        return merge(getBundles(/\.js$/).map(bundle => {
+            return gulp.src(bundle.inputFiles, { base: "." })
+                .pipe(concat(bundle.outputFileName))
+                .pipe(terser())
+                .pipe(gulp.dest("."));
+        }));
+    });
+
+gulp.task("min:css",
+    () => {
+        return merge(getBundles(/\.css$/).map(bundle => {
+            return gulp.src(bundle.inputFiles, { base: "." })
+                .pipe(concat(bundle.outputFileName))
+                .pipe(cleanCss())
+                .pipe(gulp.dest("."));
+        }));
+    });
+
+gulp.task("min", gulp.series(["min:js", "min:css"]));
+
+gulp.task("default", gulp.series(["min"]));
+
+function getBundles(regexPattern) {
+    return bundleconfig.filter(bundle => {
+        return regexPattern.test(bundle.outputFileName);
+    });
+};
