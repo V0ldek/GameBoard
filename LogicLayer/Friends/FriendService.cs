f771aab011c0464872222ee880f25ac29eb7f952
WiktorMarszalkowski <wiki.marszalkowski@gmail.com>
2019-04-07 14:23:56 +0200

    Almost completed implementation.
    Except for trasactions, speed test for search engine and overall testing if these functions work.

--- LogicLayer/Friends/FriendService.cs
+++ LogicLayer/Friends/FriendService.cs
@@ -8,6 +8,7 @@ using GameBoard.DataLayer.Enums;
 using GameBoard.LogicLayer.Friends.Dtos;
 using GameBoard.LogicLayer.UserSearch.Dtos;
 using GameBoard.DataLayer.Repositories;
+using GameBoard.LogicLayer.Friends.Exceptions;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,80 +17,102 @@ namespace GameBoard.LogicLayer.Friends
     internal /*sealed?*/ class FriendService : IFriendsService
     {
         private readonly IGameBoardRepository _repository;
-        private readonly UserManager<ApplicationUser> _userManager;
 
-        public FriendService(IGameBoardRepository repository, UserManager<ApplicationUser> userManager)
+        public FriendService(IGameBoardRepository repository)
         {
             _repository = repository;
-            _userManager = userManager;
         }
 
-        public Task<IEnumerable<UserDto>> GetFriendsByUserNameAsync(string userName)
+        public async Task<IEnumerable<UserDto>> GetFriendsByUserNameAsync(string userName)
         {
+            // I should change naming if I decide to stick to Friendship with requestedBy and requestedTo.
+            
             var user = _repository.ApplicationUsers.Where(u => u.UserName == userName);
 
-            var smallerIdThanfriends = user
-                .Include(u => u.SmallerIdInFriendships)
-                .SelectMany(u => u.SmallerIdInFriendships)
+            var smallerIdThanFriends = user
+                .Include(u => u.SentRequests)
+                .SelectMany(u => u.SentRequests)
                 .Where(f => f.FriendshipStatus == FriendshipStatus.Lasts)
-                .Include(f => f.UserGreater)
-                .Select(f => new UserDto(f.UserGreater.Id, f.UserGreater.UserName, f.UserGreater.Email));
+                .Include(f => f.RequestedTo)
+                .Select(f => new UserDto(f.RequestedTo.Id, f.RequestedTo.UserName, f.RequestedTo.Email));
 
             var greaterIdThanFriends = user
-                .Include(u => u.GreaterIdInFriendships)
-                .SelectMany(u => u.GreaterIdInFriendships)
+                .Include(u => u.ReceivedRequests)
+                .SelectMany(u => u.ReceivedRequests)
                 .Where(f => f.FriendshipStatus == FriendshipStatus.Lasts)
-                .Include(f => f.UserSmaller)
-                .Select(f => new UserDto(f.UserSmaller.Id, f.UserSmaller.UserName, f.UserSmaller.Email));
+                .Include(f => f.RequestedBy)
+                .Select(f => new UserDto(f.RequestedBy.Id, f.RequestedBy.UserName, f.RequestedBy.Email));
 
-            var allFriends = smallerIdThanfriends.Union(greaterIdThanFriends).OrderBy(u => u.UserName).ToListAsync();
+            var allFriends = await smallerIdThanFriends
+                .Union(greaterIdThanFriends)
+                .OrderBy(u => u.UserName)
+                .ToListAsync(); // can I avoid using await here?
 
             return allFriends;
         }
 
         public async Task SendFriendRequestAsync(CreateFriendRequestDto friendRequest)
         {
-            string userSmallerId;
-            string userGreaterId;
-            WhoSentLatestRequest whoSent;
-
-            if (String.Compare(friendRequest.UserNameFrom, friendRequest.UserNameTo) < 0)
-            {
-                userSmallerId = friendRequest.UserNameFrom;
-                userGreaterId = friendRequest.UserNameTo;
-                whoSent = WhoSentLatestRequest.UserWithSmallerId;
-            }
-            else
-            {
-                userSmallerId = friendRequest.UserNameTo;
-                userGreaterId = friendRequest.UserNameFrom;
-                whoSent = WhoSentLatestRequest.UserWithGreaterId;
-            }
-
-            using (var transaction = new GameBoardDbContext())
+            //using (var transaction = new GameBoardDbContext())
             {
                 var friendship = await _repository.Friendships
-                    .SingleOrDefaultAsync(f => (f.UserSmallerId == userSmallerId && f.UserSmallerId == userGreaterId));
+                    .SingleOrDefaultAsync(
+                        f => f.RequestedById == friendRequest.UserNameFrom &&
+                            f.RequestedToId == friendRequest.UserNameTo); // wrong id mixed up with userName
+
 
                 if (friendship != null)
                 {
+                    switch (friendship.FriendshipStatus)
+                    {
+                        case FriendshipStatus.PendingFriendRequest:
+                            throw new FriendRequestAlreadyPendingException("You have already sent him a friend request.");
+                        case FriendshipStatus.Lasts:
+                            throw new FriendRequestAlreadyFinalizedException("You are already friends.");
+                        case FriendshipStatus.Rejected:
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+                }
 
+                friendship = await _repository.Friendships
+                    .SingleOrDefaultAsync(
+                        f => f.RequestedById == friendRequest.UserNameTo &&
+                            f.RequestedToId == friendRequest.UserNameFrom); // wrong id mixed up with userName
+
+                if (friendship != null)
+                {
+                    switch (friendship.FriendshipStatus)
+                    {
+                        case FriendshipStatus.PendingFriendRequest:
+                            throw new FriendRequestAlreadyPendingException("You have already been invited. Go to ...here should be link..., to confirm or reject friend request.");
+                        case FriendshipStatus.Lasts:
+                            throw new FriendRequestAlreadyFinalizedException("You are already friends.");
+                        case FriendshipStatus.Rejected:
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
                 }
 
                 _repository.Friendships.Add(
                     new Friendship()
                     {
                         //auto increment
-                        UserSmallerId = friendRequest.UserNameFrom,
-                        UserGreaterId = friendRequest.UserNameTo,
+                        RequestedById = friendRequest.UserNameFrom,
+                        RequestedToId = friendRequest.UserNameTo,
                         FriendshipStatus = FriendshipStatus.PendingFriendRequest
                     });
+
+                await _repository.SaveChangesAsync();
+
+                // commit transaction
+                // what happens if an exception is thrown within the transaction?
+
             }
 
-
-            await _repository.SaveChangesAsync();
-
-            //send mail with link
+            //send email with link
         }
 
         public Task<FriendRequestDto> GetFriendRequestAsync(string friendRequestId)
@@ -98,13 +121,13 @@ namespace GameBoard.LogicLayer.Friends
             var friendship = _repository.Friendships.Where(f => f.Id == id);
 
             return friendship
-                .Include(f => f.UserSmaller)
-                .Include(f => f.UserGreater)
+                .Include(f => f.RequestedBy)
+                .Include(f => f.RequestedTo)
                 .Select(
                     f => new FriendRequestDto(
                         friendRequestId,
-                        new UserDto(f.UserSmaller.Id, f.UserSmaller.UserName, f.UserSmaller.Email),//wrong, greater can send request too.
-                        new UserDto(f.UserGreater.Id, f.UserGreater.UserName, f.UserGreater.Email),
+                        new UserDto(f.RequestedBy.Id, f.RequestedBy.UserName, f.RequestedBy.Email),//wrong, greater can send request too.
+                        new UserDto(f.RequestedTo.Id, f.RequestedTo.UserName, f.RequestedTo.Email),
                         f.FriendshipStatus.ToFriendRequest()))
                 .SingleAsync();
         }
--- LogicLayer/UserSearch/UserSearchServiceTests.cs
+++ LogicLayer/UserSearch/UserSearchServiceTests.cs
@@ -1,72 +1,72 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Text;
-using GameBoard.DataLayer.Entities;
-using GameBoard.DataLayer.Repositories;
-using Xunit;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Drawing;
+//using System.Text;
+//using GameBoard.DataLayer.Entities;
+//using GameBoard.DataLayer.Repositories;
+//using Xunit;
 
-namespace GameBoard.LogicLayer.UserSearch
-{
-    public class UserSearchServiceTests
-    {
-        public static TheoryData<ApplicationUser> UserData => new TheoryData<ApplicationUser>
-            {
-                {
-                    new ApplicationUser();
-                },
-                {
-                    new Duck("Jacuś", Color.Red);
-                },
-                {
-                    new Duck("Yellow", Color.Yellow);
-                },
-                {
-                    new Duck("Yellow", Color.Green);
-                }
-            }
+//namespace GameBoard.LogicLayer.UserSearch
+//{
+//    public class UserSearchServiceTests
+//    {
+//        public static TheoryData<ApplicationUser> UserData => new TheoryData<ApplicationUser>
+//            {
+//                {
+//                    new ApplicationUser();
+//                },
+//                {
+//                    new Duck("Jacuś", Color.Red);
+//                },
+//                {
+//                    new Duck("Yellow", Color.Yellow);
+//                },
+//                {
+//                    new Duck("Yellow", Color.Green);
+//                }
+//            }
 
-        private const string ConnectionString = "UserID=posgres;Password=postgres;Host=localhost;Port=5432;Database=duck_db";
+//        private const string ConnectionString = "UserID=posgres;Password=postgres;Host=localhost;Port=5432;Database=duck_db";
 
-        [Fact]
-        public void SearchEngineSpeedTest()
-        {
-            ARRANGE
-            using (IGameBoardRepository repository = new GameBoardDbContext(ConnectionString))
-            {
-                repository.ApplicationUsers.Add(new ApplicationUser { UserName = "ZiOmEk", Email = "jj@jj" });
-                var search = new UserSearchService(repository);
+//        [Fact]
+//        public void SearchEngineSpeedTest()
+//        {
+//            //ARRANGE
+//            using (IGameBoardRepository repository = new GameBoardDbContext())
+//            {
+//                repository.ApplicationUsers.Add(new ApplicationUser { UserName = "ZiOmEk", Email = "jj@jj" });
+//                var search = new UserSearchService(repository);
 
-                var expectedName = duck.Name;
-                var color = duck.Color;
+//                var expectedName = duck.Name;
+//                var color = duck.Color;
 
-                repository.GetRandomDuck().Returns(duck);
+//                repository.GetRandomDuck().Returns(duck);
 
-                var systemUnderTest = new DuckGuesser(repository);
+//                var systemUnderTest = new DuckGuesser(repository);
 
-                ACT
+//                //ACT
 
-               var resultName = systemUnderTest.GuessColor(color);
+//                var resultName = systemUnderTest.GuessColor(color);
 
-                ASSERT
-                Assert.Equal(expectedName, resultName);
-            }
-        }
+//                //ASSERT
+//                Assert.Equal(expectedName, resultName);
+//            }
+//        }
 
-        [Theory]
-        [MemberData(nameof(DuckData))]
-        public void GuessColor_WhenColorDoesNotMatchWithGivenDuck_ThrowsApplicationException(Duck duck)
-        {
-            ARRANGE
-           var color = duck.Color == Color.Yellow ? Color.Green : Color.Yellow;
+//        [Theory]
+//        [MemberData(nameof(DuckData))]
+//        public void GuessColor_WhenColorDoesNotMatchWithGivenDuck_ThrowsApplicationException(Duck duck)
+//        {
+//            ARRANGE
+//           var color = duck.Color == Color.Yellow ? Color.Green : Color.Yellow;
 
-            var repository = Substitute.For<IRepository>();
-            repository.GetRandomDuck().Returns(duck);
+//            var repository = Substitute.For<IRepository>();
+//            repository.GetRandomDuck().Returns(duck);
 
-            var systemUnderTest = new DuckGuesser(repository);
+//            var systemUnderTest = new DuckGuesser(repository);
 
-            ACT & ASSERT
-            Assert.Throws<ApplicationException>(() => systemUnderTest.GuessColor(color));
-        }
-    }
-}
+//            ACT & ASSERT
+//            Assert.Throws<ApplicationException>(() => systemUnderTest.GuessColor(color));
+//        }
+//    }
+//}
