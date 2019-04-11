709a6eee4c35c67155cd368840ce8a66c640ef94
matus3000 <matus3000@gmail.com>
2019-04-11 13:51:59 +0200

    Update Startup.cs

--- GameBoard/Startup.cs
+++ GameBoard/Startup.cs
@@ -58,7 +58,7 @@ namespace GameBoard
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsStaging())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseDatabaseErrorPage();
