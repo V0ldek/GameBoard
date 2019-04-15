479ece49f9aa98284f8da5e34e69d048a8bbac60
WiktorMarszalkowski <wiki.marszalkowski@gmail.com>
2019-04-15 17:12:57 +0200

    Set transaction in the trigger to serializable to resolve concurrency issues.

--- DataLayer/Migrations/Scripts/20190414094415_AddedApplicationUserAndFriendship_Up.sql
+++ DataLayer/Migrations/Scripts/20190414094415_AddedApplicationUserAndFriendship_Up.sql
@@ -7,45 +7,48 @@ CREATE TRIGGER uniquelastingorpendingfriendship
   INSTEAD OF INSERT
 AS
   BEGIN
-    DECLARE @friendshipstatus INT;
-    SET @friendshipstatus =
-    (SELECT friendships.friendshipstatus
-     FROM friendships,
-          inserted
-     WHERE inserted.requestedtoid = friendships.requestedbyid
-       AND inserted.requestedbyid = friendships.requestedtoid);
-    -- Checking if RequestedTo have already sent an friend request to RequestedBy.
-    IF @friendshipstatus = 0
-      THROW 50000, 'PendingFromRequestedTo', 1;
-    IF @friendshipstatus = 2
-      THROW 50002, 'Lasts', 1;
-    -- @friendshipStatus IS NULL or @friendshipStatus = 1, so we can proceed.
-    SET @friendshipstatus =
-    (SELECT friendships.friendshipstatus
-     FROM friendships,
-          inserted
-     WHERE inserted.requestedbyid = friendships.requestedbyid
-       AND inserted.requestedtoid = friendships.requestedtoid);
+    SET TRANSACTION ISOLATION LEVEL SERIALIZABLE
+    BEGIN TRANSACTION
+      DECLARE @friendshipstatus INT;
+      SET @friendshipstatus =
+      (SELECT friendships.friendshipstatus
+       FROM friendships,
+            inserted
+       WHERE inserted.requestedtoid = friendships.requestedbyid
+         AND inserted.requestedbyid = friendships.requestedtoid);
+      -- Checking if RequestedTo have already sent an friend request to RequestedBy.
+      IF @friendshipstatus = 0
+        THROW 50000, 'PendingFromRequestedTo', 1;
+      IF @friendshipstatus = 2
+        THROW 50002, 'Lasts', 1;
+      -- @friendshipStatus IS NULL or @friendshipStatus = 1, so we can proceed.
+      SET @friendshipstatus =
+      (SELECT friendships.friendshipstatus
+       FROM friendships,
+            inserted
+       WHERE inserted.requestedbyid = friendships.requestedbyid
+         AND inserted.requestedtoid = friendships.requestedtoid);
 
-    IF @friendshipstatus IS NULL
-      INSERT INTO friendships (requestedbyid, requestedtoid, friendshipstatus)
-       (SELECT requestedbyid, requestedtoid, friendshipstatus
-        FROM inserted); -- better way to do this? the trigger isn't called recursively.
-    ELSE IF @friendshipstatus = 0
-      THROW 50001, 'PendingFromRequestedBy', 1;
-    ELSE IF @friendshipstatus = 2
-      THROW 50002, 'Lasts', 1;
-    ELSE IF @friendshipstatus = 1
-      UPDATE friendships
-      SET friendships.friendshipstatus = inserted.friendshipstatus
-      FROM inserted
-      WHERE inserted.requestedbyid = friendships.requestedbyid
-        AND inserted.requestedtoid = friendships.requestedtoid; -- code repetition, better way TO do this?
-ELSE THROW 50004, 'NotSupportedValue', 1;
-SELECT id
-FROM friendships f
-WHERE @@ROWCOUNT > 0
-  AND f.id = scope_identity();
+      IF @friendshipstatus IS NULL
+        INSERT INTO friendships (requestedbyid, requestedtoid, friendshipstatus)
+         (SELECT requestedbyid, requestedtoid, friendshipstatus
+          FROM inserted); -- better way to do this? the trigger isn't called recursively.
+      ELSE IF @friendshipstatus = 0
+        THROW 50001, 'PendingFromRequestedBy', 1;
+      ELSE IF @friendshipstatus = 2
+        THROW 50002, 'Lasts', 1;
+      ELSE IF @friendshipstatus = 1
+        UPDATE friendships
+        SET friendships.friendshipstatus = inserted.friendshipstatus
+        FROM inserted
+        WHERE inserted.requestedbyid = friendships.requestedbyid
+          AND inserted.requestedtoid = friendships.requestedtoid; -- code repetition, better way TO do this?
+      ELSE THROW 50004, 'NotSupportedValue', 1;
+      SELECT id
+      FROM friendships f
+      WHERE @@ROWCOUNT > 0
+        AND f.id = scope_identity();
+	COMMIT;
 END;
 
 GO
