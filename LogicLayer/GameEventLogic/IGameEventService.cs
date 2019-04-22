6d830b5a8c7d63186ca0e094e5a3cbff73210342
WiktorMarszalkowski <wiki.marszalkowski@gmail.com>
2019-04-22 21:30:10 +0200

    Added comments.

--- LogicLayer/GameEventLogic/IGameEventService.cs
+++ LogicLayer/GameEventLogic/IGameEventService.cs
@@ -10,10 +10,10 @@ namespace GameBoard.LogicLayer.GameEventLogic
     // Perhaps this interface should be split into three separate interfaces.
     public interface IGameEventService
     {
-        //Throws CreateGameEventException when new GameEvent could not be created
+        // Throws CreateGameEventException when new GameEvent could not be created (what can go wrong?)
         Task CreateGameEventAsync([NotNull] CreateGameEventDto requestedGameEvent, [NotNull] IEnumerable<string> games);
         
-        //Throws DeleteGameEventException when GameEvent could not be deleted
+        // Throws DeleteGameEventException when GameEvent could not be deleted (Event with such id doesn't exist, but I don't know if that is actually possible)
         Task DeleteGameEventAsync([NotNull] string gameEventId);
 
         Task EditGameEventAsync(
@@ -26,6 +26,8 @@ namespace GameBoard.LogicLayer.GameEventLogic
 
         Task<GameEventPermission> GetGameEventParticipationByUserAsync([NotNull] string gameEventId, [NotNull] string userId);
 
+        // Returns Task with null if game event does not exist.
+        [ItemCanBeNull]
         Task<GameEventDto> GetGameEventAsync([NotNull] string gameEventId);
 
         Task SendGameEventInvitationAsync([NotNull] string gameEventId, [NotNull] string userId);
