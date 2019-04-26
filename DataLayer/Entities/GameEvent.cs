ccbc56fffcd93c8541895bdd39f01a3a5320e63c
matus3000 <matus3000@gmail.com>
2019-04-26 14:10:04 +0200

    Added field EventName

--- DataLayer/Entities/GameEvent.cs
+++ DataLayer/Entities/GameEvent.cs
@@ -9,6 +9,7 @@ namespace GameBoard.DataLayer.Entities
     public class GameEvent
     {
         public string Id { get; set; }
+        public string EventName { get; set; }
         public DateTime MeetingTime { get; set; }
         public string Place { get; set; }
 
