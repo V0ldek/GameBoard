767b2172d0b79c26b01107245c07c0908403a1c7
RAPTOR\misza <zexter2@wp.pl>
2019-04-28 15:14:21 +0200

    Cleanup.

--- LogicLayer/Notifications/MailSender.cs
+++ LogicLayer/Notifications/MailSender.cs
@@ -1,22 +1,79 @@
-﻿using Microsoft.Extensions.Options;
-using System.Threading.Tasks;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
+using System.Threading.Tasks;
+using GameBoard.LogicLayer.Configurations;
+using Microsoft.Extensions.Options;
 using SendGrid;
 using SendGrid.Helpers.Mail;
-using GameBoard.LogicLayer.Configurations;
 
 namespace GameBoard.LogicLayer.Notifications
 {
     public class MailSender : IMailSender
     {
+        private MailNotificationsConfiguration MailOptions { get; }
+
         public MailSender(IOptions<MailNotificationsConfiguration> mailOptionsAccessor)
         {
             MailOptions = mailOptionsAccessor.Value;
         }
 
-        private MailNotificationsConfiguration MailOptions { get; }
+        public Task SendEmailConfirmationAsync(string email, string link)
+        {
+            var notification = new Notification(
+                GetHtmlPath(MailOptions.EmailConfirmationHtml),
+                link,
+                "Email Confirmation");
+            return SendEmailAsync(email, notification);
+        }
+
+        public Task SendEventCancellationAsync(IEnumerable<string> emails, string link)
+        {
+            var notification = new Notification(
+                GetHtmlPath(MailOptions.EventCancellationHtml),
+                link,
+                "Event Cancellation");
+            return SendEmailAsync(emails, notification);
+        }
+
+        public Task SendEventConfirmationAsync(IEnumerable<string> emails, string link)
+        {
+            var notification = new Notification(
+                GetHtmlPath(MailOptions.EventConfirmationHtml),
+                link,
+                "Event Confirmation");
+            return SendEmailAsync(emails, notification);
+        }
+
+        public Task SendEventInvitationAsync(IEnumerable<string> emails, string link)
+        {
+            var notification = new Notification(GetHtmlPath(MailOptions.EventInvitationHtml), link, "Event Invitation");
+            return SendEmailAsync(emails, notification);
+        }
+
+        public Task SendFriendAcceptAsync(string email, string link)
+        {
+            var notification = new Notification(
+                GetHtmlPath(MailOptions.FriendAcceptHtml),
+                link,
+                "Friend Invitation Accepted");
+            return SendEmailAsync(email, notification);
+        }
+
+        public Task SendFriendInvitationAsync(string email, string link)
+        {
+            var notification = new Notification(
+                GetHtmlPath(MailOptions.FriendInvitationHtml),
+                link,
+                "Friend Invitation");
+            return SendEmailAsync(email, notification);
+        }
+
+        public Task SendPasswordResetAsync(string email, string link)
+        {
+            var notification = new Notification(GetHtmlPath(MailOptions.PasswordResetHtml), link, "Password Reset");
+            return SendEmailAsync(email, notification);
+        }
 
         private Task SendEmailAsync(IEnumerable<string> emails, Notification notification)
             => Execute(MailOptions.SendGridApiKey, emails, notification);
@@ -38,7 +95,7 @@ namespace GameBoard.LogicLayer.Notifications
             var subject = notification.Subject;
             var htmlContent = notification.Html;
 
-            foreach (string email in emails)
+            foreach (var email in emails)
             {
                 tos.Add(new EmailAddress(email));
             }
@@ -50,51 +107,9 @@ namespace GameBoard.LogicLayer.Notifications
 
         private string GetHtmlPath(string htmlTemplateName)
         {
-            string assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            string htmlPath = Path.Combine(assemblyPath, MailOptions.DefaultHtmlPath, htmlTemplateName);
+            var assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var htmlPath = Path.Combine(assemblyPath, MailOptions.DefaultHtmlPath, htmlTemplateName);
             return htmlPath;
-        }
-
-        public Task SendEmailConfirmationAsync(string email, string link)
-        {
-            Notification notification = new Notification(GetHtmlPath(MailOptions.EmailConfirmationHtml), link, "Email Confirmation");
-            return SendEmailAsync(email, notification);
-        }
-
-        public Task SendEventCancellationAsync(IEnumerable<string> emails, string link)
-        {
-            Notification notification = new Notification(GetHtmlPath(MailOptions.EventCancellationHtml), link, "Event Cancellation");
-            return SendEmailAsync(emails, notification);
-        }
-
-        public Task SendEventConfirmationAsync(IEnumerable<string> emails, string link)
-        {
-            Notification notification = new Notification(GetHtmlPath(MailOptions.EventConfirmationHtml), link, "Event Confirmation");
-            return SendEmailAsync(emails, notification);
-        }
-
-        public Task SendEventInvitationAsync(IEnumerable<string> emails, string link)
-        {
-            Notification notification = new Notification(GetHtmlPath(MailOptions.EventInvitationHtml), link, "Event Invitation");
-            return SendEmailAsync(emails, notification);
-        }
-
-        public Task SendFriendAcceptAsync(string email, string link)
-        {
-            Notification notification = new Notification(GetHtmlPath(MailOptions.FriendAcceptHtml), link, "Friend Invitation Accepted");
-            return SendEmailAsync(email, notification);
-        }
-
-        public Task SendFriendInvitationAsync(string email, string link)
-        {
-            Notification notification = new Notification(GetHtmlPath(MailOptions.FriendInvitationHtml), link, "Friend Invitation");
-            return SendEmailAsync(email, notification);
-        }
-
-        public Task SendPasswordResetAsync(string email, string link)
-        {
-            Notification notification = new Notification(GetHtmlPath(MailOptions.PasswordResetHtml), link, "Password Reset");
-            return SendEmailAsync(email, notification);
         }
     }
 }
