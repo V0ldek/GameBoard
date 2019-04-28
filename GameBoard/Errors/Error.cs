fe8d5a94c38a19a9184066aeed8525707863a013
V0ldek <registermen@gmail.com>
2019-04-28 18:49:48 +0200

    Unified error handling for Controllers and Pages.

---
+++ GameBoard/Errors/Error.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GameBoard.Errors
+{
+    internal static class Error
+    {
+        public static IErrorHandlingWrapper FromController(Controller controller) => new ErrorHandlingWrapper(controller);
+
+        public static IErrorHandlingWrapper FromPage(PageModel pageModel) => new ErrorHandlingWrapper(pageModel);
+    }
+}
---
+++ GameBoard/Errors/IErrorHandlingWrapper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GameBoard.Errors
+{
+    internal interface IErrorHandlingWrapper
+    {
+        LocalRedirectResult AccessDenied(string handlerLocalUri = "/Identity/Account/AccessDenied");
+
+        ViewResult Error(string title, string message, HttpStatusCode statusCode, ILogger logger = null);
+
+        JsonResult ErrorJson(string title, string message, HttpStatusCode statusCode, ILogger logger = null);
+    }
+}
