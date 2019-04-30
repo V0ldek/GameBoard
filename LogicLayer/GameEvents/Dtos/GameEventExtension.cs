c100338c43bd31fd0bd5eaba5f07917a2c5a52e6
WiktorMarszalkowski <wiki.marszalkowski@gmail.com>
2019-04-30 19:54:40 +0200

    Added GameEventExtension.

--- LogicLayer/GameEvents/Dtos/GameEventExtension.cs
+++ LogicLayer/GameEvents/Dtos/GameEventExtension.cs
@@ -1,25 +1,45 @@
-﻿using GameBoard.DataLayer.Entities;
+﻿using System.Collections;
+using GameBoard.DataLayer.Entities;
 using GameBoard.LogicLayer.UserSearch.Dtos;
-using System.Threading.Tasks;
 using System.Linq;
+using GameBoard.DataLayer.Enums;
+using System.Collections.Generic;
 
-
-
-namespace GameBoard.LogicLayer.GameEventLogic.Dtos
+namespace GameBoard.LogicLayer.GameEvents.Dtos
 {
     public static class GameEventExtension
     {
+        private static IEnumerable<UserDto> GetUsersWithSamePartitipationStatus(
+            this GameEvent gameEvent,
+            ParticipationStatus participationStatus)
+        {
+            return gameEvent.Participations
+                .Where(p => p.ParticipationStatus == participationStatus)
+                .Select(p => p.Paticipant.ToUserDto());
+        }
+
         public static GameEventDto ToGameEventDto(this GameEvent gameEvent)
         {
             return new GameEventDto(
-            gameEvent.Id.ToString(),
-            gameEvent.EventName.ToString(),
-            gameEvent.MeetingTime,
-            gameEvent.Place.ToString(),
-            gameEvent.Games.Select(g => new string(g.Name)),
-            gameEvent.Participations.Select(inv => inv.Paticipant.ToUserDto())
+                gameEvent.Id,
+                gameEvent.EventName, // strings are immutable so we don't have to create a new instance of the same object by ourselves
+                gameEvent.MeetingTime,
+                gameEvent.Place, // strings are immutable so we don't have to create a new instance of the same object by ourselves
+                gameEvent.Games.Select(g => g.Name), // strings are immutable so we don't have to create a new instance of the same object by ourselves
+                gameEvent.GetUsersWithSamePartitipationStatus(ParticipationStatus.Creator).Single(), //not sure if that is correct. Does include do the job?
+                gameEvent.GetUsersWithSamePartitipationStatus(ParticipationStatus.PendingGuest),
+                gameEvent.GetUsersWithSamePartitipationStatus(ParticipationStatus.AcceptedGuest)
             );
         }
- 
+
+        public static GameEventListItemDto ToGameEventListItemDto(this GameEvent gameEvent)
+        {
+            return new GameEventListItemDto(
+                gameEvent.Id, 
+                gameEvent.EventName, 
+                gameEvent.GetUsersWithSamePartitipationStatus(ParticipationStatus.Creator).Single()
+            );
+        }
     }
 }
+
