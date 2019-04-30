df9cca50f9112df394bf18e83b3d5d3aeb914190
WiktorMarszalkowski <wiki.marszalkowski@gmail.com>
2019-04-30 18:12:06 +0200

    Resolved comments.

--- LogicLayer/GameEvents/IGameEventService.cs
+++ LogicLayer/GameEvents/IGameEventService.cs
@@ -5,13 +5,10 @@ using JetBrains.Annotations;
 
 namespace GameBoard.LogicLayer.GameEvents
 {
-    // Perhaps this interface should be split into three separate interfaces.
     public interface IGameEventService
     {
-        // Throws CreateGameEventException when new GameEvent could not be created (what can go wrong?)
         Task CreateGameEventAsync([NotNull] CreateGameEventDto requestedGameEvent);
 
-        // Throws DeleteGameEventException when GameEvent could not be deleted (Event with such id doesn't exist, but I don't know if that is actually possible)
         Task DeleteGameEventAsync(int gameEventId);
 
         Task EditGameEventAsync([NotNull] EditGameEventDto editedEvent);
