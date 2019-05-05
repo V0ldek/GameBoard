d411a00ba537a4c6b02fc97bc421c9d0437812e3
RAPTOR\misza <zexter2@wp.pl>
2019-05-05 23:06:50 +0200

    Created basic architecture.

---
+++ GameBoard/Controllers/GroupsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using GameBoard.LogicLayer.Groups;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameBoard.Controllers
+{
+    [Authorize]
+    public class GroupsController : Controller
+    {
+        private readonly IGroupsService _groupsService;
+
+        public GroupsController(IGroupsService groupsService)
+        {
+            _groupsService = groupsService;
+        }
+    }
+}
