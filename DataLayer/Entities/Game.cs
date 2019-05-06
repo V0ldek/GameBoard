b426a0a0f33db41e78599c6758f39b1b4bdfb902
WiktorMarszalkowski <wiki.marszalkowski@gmail.com>
2019-05-06 13:00:55 +0200

    Added comment about PositionOnTheList.

--- DataLayer/Entities/Game.cs
+++ DataLayer/Entities/Game.cs
@@ -9,6 +9,6 @@
         public int GameEventId { get; set; }
         public GameEvent GameEvent { get; set; }
 
-        public int? PositionOnTheList { get; set; }
+        public int? PositionOnTheList { get; set; } // equals null if the game has been removed from the list.
     }
 }
