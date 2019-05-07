23ae7bccf9445d217b1c9375b713bc9f70cbba7b
RAPTOR\misza <zexter2@wp.pl>
2019-05-07 10:44:29 +0200

    Added add group funcationality

--- GameBoard/Views/Shared/Components/GroupAddBox/GroupAddBoxViewComponent.cs
+++ GameBoard/Views/Shared/Components/GroupAddBox/GroupAddBoxViewComponent.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using GameBoard.Models.Groups;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GameBoard.Views.Shared.Components.GroupAddBox
