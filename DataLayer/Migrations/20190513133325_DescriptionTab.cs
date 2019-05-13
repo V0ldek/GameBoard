17a8f87460bbd5a659850be4d929b48a0574298b
matus3000 <matus3000@gmail.com>
2019-05-13 15:41:18 +0200

    DataLayer Changes

---
+++ DataLayer/Migrations/20190513133325_DescriptionTab.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GameBoard.DataLayer.Migrations
+{
+    public partial class DescriptionTab : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DescriptionTab",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    GameEventId = table.Column<int>(nullable: false),
+                    Description = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DescriptionTab", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DescriptionTab_GameEvent_GameEventId",
+                        column: x => x.GameEventId,
+                        principalTable: "GameEvent",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DescriptionTab_GameEventId",
+                table: "DescriptionTab",
+                column: "GameEventId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DescriptionTab");
+        }
+    }
+}
---
+++ DataLayer/Migrations/Scripts/20190513133325_DescriptionTab_Down.sql
@@ -0,0 +1,29 @@
+﻿DROP TRIGGER IF EXISTS TR_GameEvent_InsteadOfDelete;
+
+GO
+
+-- We need to have old trigger in the database.
+CREATE TRIGGER TR_GameEvent_InsteadOfDelete
+  ON GameEvent
+  INSTEAD OF DELETE
+AS
+  BEGIN
+    DELETE FROM GameEventParticipation
+    FROM DELETED
+    WHERE GameEventParticipation.TakesPartInId = DELETED.Id;
+
+    DELETE FROM Game -- This DELETE could be replaced with CascadeDelete on Games, but it seems more consistent to put everything in one place.
+    FROM DELETED
+    WHERE Game.GameEventId = DELETED.Id;
+
+	DELETE FROM DescriptionTab
+	FROM DELETED
+	WHERE DescriptionTab.GameEventId = DELETED.Id;
+
+    DELETE FROM GameEvent
+    FROM DELETED
+    WHERE GameEvent.Id = DELETED.Id;
+
+  END;
+
+GO
