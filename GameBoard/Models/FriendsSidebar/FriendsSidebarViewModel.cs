596e3ccc1e851449029e4c48bf88181a979079e2
RAPTOR\misza <zexter2@wp.pl>
2019-05-14 12:34:19 +0200

    I have changes, can't remember what kind, gotta merge

---
+++ GameBoard/Models/FriendsSidebar/FriendsSidebarViewModel.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Collections.Generic;
+using GameBoard.Models.Groups;
+using GameBoard.Models.User;
+
+namespace GameBoard.Models.FriendsSidebar
+{
+    public class FriendsSidebarViewModel
+    {
+        public IEnumerable<GroupViewModel> Groups { get; }
+        public IEnumerable<UserViewModel> Users { get; }
+
+        public FriendsSidebarViewModel(IEnumerable<GroupViewModel> groups, IEnumerable<UserViewModel> users)
+        {
+            Groups = groups;
+            Users = users;
+        }
+    }
+}
