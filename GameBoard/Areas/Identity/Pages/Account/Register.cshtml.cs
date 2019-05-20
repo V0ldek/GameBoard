a365113fe74669f263a90004b2fa6f9df238f320
V0ldek <registermen@gmail.com>
2019-05-20 02:26:20 +0200

    Transactional safety in Register.

--- GameBoard/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ GameBoard/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using System.Transactions;
 using GameBoard.Areas.Identity.Pages.Account.Notifications;
 using GameBoard.DataLayer.Entities;
 using GameBoard.LogicLayer.Notifications;
@@ -40,13 +41,19 @@ namespace GameBoard.Areas.Identity.Pages.Account
             }
 
             var user = new ApplicationUser {UserName = Input.UserName, Email = Input.Email};
-            var result = await _userManager.CreateAsync(user, Input.Password);
-            if (result.Succeeded)
-            {
-                _logger.LogInformation("User created a new account with password.");
-                SendRegistrationEmail(user);
 
-                return RedirectToPage("/Account/ConfirmEmailInfo");
+            IdentityResult result;
+            using (var transaction = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+            {
+                result = await _userManager.CreateAsync(user, Input.Password);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("User created a new account with password.");
+                    SendRegistrationEmail(user);
+                    transaction.Complete();
+
+                    return RedirectToPage("/Account/ConfirmEmailInfo");
+                }
             }
 
             foreach (var error in result.Errors)
