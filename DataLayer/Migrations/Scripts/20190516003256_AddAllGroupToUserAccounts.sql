5a5e218e6e3474f74fe778d3df86ce6c7fe6093e
V0ldek <registermen@gmail.com>
2019-05-20 23:57:04 +0200

    Styling in SQL and JSON files.

--- DataLayer/Migrations/Scripts/20190516003256_AddAllGroupToUserAccounts.sql
+++ DataLayer/Migrations/Scripts/20190516003256_AddAllGroupToUserAccounts.sql
@@ -9,23 +9,24 @@ FROM [User];
 DECLARE @UsersWithAllGroupId TABLE
 (
 	UserId NVARCHAR(450),
-	AllGroupId int
+	AllGroupId INT
 )
-
--- we don't need to join on [Group].Name = 'All', because all users own only one group
 
 INSERT INTO @UsersWithAllGroupId (UserId, AllGroupId)
 SELECT [User].Id, [Group].Id
-FROM [User] JOIN [Group] ON [User].Id = [Group].OwnerId;
+FROM [User] 
+JOIN [Group] ON [User].Id = [Group].OwnerId AND [Group].Name = 'All';
 
 --Add all friends that friendship were requested by friends of users
 
 INSERT INTO [GroupUser] (GroupId, UserId)
 SELECT UsersWithAllGroupId.AllGroupId, [Friendship].RequestedById
-FROM @UsersWithAllGroupId as UsersWithAllGroupId JOIN [Friendship] ON UsersWithAllGroupId.UserId = [Friendship].RequestedToId;
+FROM @UsersWithAllGroupId AS UsersWithAllGroupId 
+JOIN [Friendship] ON UsersWithAllGroupId.UserId = [Friendship].RequestedToId;
 
 --Add all friends that friendship were requested by users
 
 INSERT INTO [GroupUser] (GroupId, UserId)
 SELECT UsersWithAllGroupId.AllGroupId, [Friendship].RequestedToId
-FROM @UsersWithAllGroupId as UsersWithAllGroupId JOIN [Friendship] ON UsersWithAllGroupId.UserId = [Friendship].RequestedById;
+FROM @UsersWithAllGroupId AS UsersWithAllGroupId 
+JOIN [Friendship] ON UsersWithAllGroupId.UserId = [Friendship].RequestedById;
