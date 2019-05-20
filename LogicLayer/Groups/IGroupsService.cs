9d9fea31b1b9e5e1c62383e686f4f99c99486bbb
RAPTOR\misza <zexter2@wp.pl>
2019-05-20 23:59:47 +0200

    Removed unnecessary comments

--- LogicLayer/Groups/IGroupsService.cs
+++ LogicLayer/Groups/IGroupsService.cs
@@ -7,17 +7,14 @@ namespace GameBoard.LogicLayer.Groups
 {
     public interface IGroupsService
     {
-        // Gets groups created by this user(userName).
         [NotNull]
         [ItemNotNull]
         Task<IEnumerable<GroupDto>> GetGroupsByUserNameAsync([NotNull] string userName);
 
         Task<GroupDto> GetGroupByNamesAsync([NotNull] string owner, [NotNull] string groupName);
 
-        // Adds new group created by user(userName).
         Task AddGroupAsync([NotNull] string userName, [NotNull] string groupName);
 
-        // Adds new user to group(groupId).
         Task AddUserToGroupAsync([NotNull] string userName, int groupId);
     }
 }
