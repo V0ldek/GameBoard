5a8fddfc3d1d9825e25d39cbd07098989344ce12
RAPTOR\misza <zexter2@wp.pl>
2019-05-20 23:52:44 +0200

    Removed miniature argument

--- GameBoard/Views/Shared/Components/FriendSearchBox/FriendSearchBoxViewComponent.cs
+++ GameBoard/Views/Shared/Components/FriendSearchBox/FriendSearchBoxViewComponent.cs
@@ -5,7 +5,7 @@ namespace GameBoard.Views.Shared.Components.GroupCard
 {
     public class FriendSearchBoxViewComponent : ViewComponent
     {
-        public IViewComponentResult Invoke(GroupViewModel group, bool miniature = false) =>
+        public IViewComponentResult Invoke(GroupViewModel group) =>
             View("FriendsSearchBox", group);
     }
 }
